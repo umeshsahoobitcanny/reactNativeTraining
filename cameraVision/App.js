34da11e75e0a4305e776f252b42e6e47af0592d3
umeshsahoo <umesh.sahoo@bitcanny.com>
2022-12-06 14:55:40 +0530

    add

---
+++ cameraVision/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+const Vis = () => {
+  return (
+    <View style={styles.screen}>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.topBar}>
+          <Text style={styles.topBarTitleText}>React Native Scanner</Text>
+        </View>
+      </SafeAreaView>
+      <View style={styles.caption}>
+        <Text style={styles.captionTitleText}>
+          {' '}
+          Welcome To React-Native-Camera Tutorial
+        </Text>
+      </View>
+      <RNCamera style={styles.rnCamera} />
+      <View style={styles.cameraControl}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>New QR Scan</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  topBar: {
+    height: 50,
+    backgroundColor: '#62d1bc', // green
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topBarTitleText: {
+    color: '#ffffffff', // white
+    fontSize: 20,
+  },
+  caption: {
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  captionTitleText: {
+    color: '#121B0D', // black
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
+export default Vis;
